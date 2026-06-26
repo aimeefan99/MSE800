3786a537299a2331429bc20d4cc42c09c10ead92
Fan Fan <fanfanwh@cn.ibm.com>
2026-06-27 08:56:36 +1200

    Week 12 - Activity 1.3: Flask

---
+++ MSE800-PSD/Week12/Activity 1.3: Flask/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/") 
+def hello_flask():
+    return "<p>Hello, Flask!</p>"
+
+@app.route("/bye") 
+def bye_flask():
+    return "<p>Bye, Flask!</p>"
+
+@app.route("/username/<name>") 
+def learn(name):
+    return f"{name} is learning Flask!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
