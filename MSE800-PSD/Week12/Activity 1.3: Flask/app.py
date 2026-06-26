de210bb43741bcbdfd5c6e423319cbeca14fe9b9
Fan Fan <fanfanwh@cn.ibm.com>
2026-06-27 10:10:13 +1200

    Week 12 - Activity 3: CSS - inline style

--- MSE800-PSD/Week12/Activity 1.3: Flask/app.py
+++ MSE800-PSD/Week12/Activity 1.3: Flask/app.py
@@ -1,17 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/") 
+@app.route("/")
 def hello_flask():
-    return "<p>Hello, Flask!</p>"
+    return render_template("index.html")
 
-@app.route("/bye") 
+@app.route("/bye")
 def bye_flask():
     return "<p>Bye, Flask!</p>"
 
-@app.route("/username/<name>") 
+@app.route("/username/<name>")
 def learn(name):
     return f"{name} is learning Flask!"
 
+@app.route("/<name>/<int:number>")
+def learn_time(name, number):
+    return f"{name} is learning Flask! She wakes up at {number} every day"
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
